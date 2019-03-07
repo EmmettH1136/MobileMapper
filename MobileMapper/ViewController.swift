7dc54bf8b1d78bdd03bcba68cb40284128075ac6
Emmett <Ehasley1136@stu.d214.org>
2019-03-07 11:34:55 -0600

    added allow location

--- MobileMapper/ViewController.swift
+++ MobileMapper/ViewController.swift
@@ -11,9 +11,12 @@ import MapKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
-    
+    let locationManager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager.requestWhenInUseAuthorization()
+        mapView.showsUserLocation = true
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
