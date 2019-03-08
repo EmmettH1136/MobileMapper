f78ce366e71f3696197a6ed43aff38472b48a0d5
Emmett <Ehasley1136@stu.d214.org>
2019-03-08 11:24:42 -0600

    added zoom functionality

--- MobileMapper/ViewController.swift
+++ MobileMapper/ViewController.swift
@@ -28,6 +28,11 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         print(currentLocation)
     }
     @IBAction func whenZoomButtonPressed(_ sender: Any) {
+        print("pause")
+        let center = currentLocation.coordinate
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: center, span: span)
+        mapView.setRegion(region, animated: true)
     }
     @IBAction func whenSearchButtonPressed(_ sender: Any) {
     }
